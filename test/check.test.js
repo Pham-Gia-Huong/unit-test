8e236c308767f82429aebeb74bb238ff2a4c48ce
Huong Pham <phamgiahuong2912@gmail.com>
2019-09-06 17:04:01 +0700

    abc

--- test/check.test.js
+++ test/check.test.js
@@ -1,4 +1,8 @@
+const path = require("path")
+console.log(path.resolve("../build/isCheck"));
+
 let build = require("../build/isCheck");
+
 test("check data",()=>{
   expect(build.check()).toEqual([1,2]);
 })
